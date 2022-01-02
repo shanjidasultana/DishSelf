b1e8dcdf85aba2d8c7daedd411486910daebcbc1
shanjidasultana <shanjidasultana2000@gmail.com>
2022-01-02 21:44:52 +0600

    'commit'

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ click }) => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <h2>MERN Shopping Cart</h2>
+        <h2>MERN Shopping Cart mmmmmmm</h2>
       </div>
 
       <ul className="navbar__links">
